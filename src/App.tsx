a46700c838ebc48fa181c8110f201d688b5b12ac
Marcos Portela <marcosnasp@gmail.com>
2023-10-06 14:01:53 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>GitHub Finder</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
